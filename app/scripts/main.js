01d598371e9d89320cb08a01f6bf60b062922fdd
ierso <coryierso@gmail.com>
2017-04-21 21:39:40 -0400

    added slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:false
+        }
+    }
+})
